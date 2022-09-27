82cd465bb272268c7c4b5f8023fa4a84f37bb433
Mark Angelo Letada <mrkngl01@gmail.com>
2022-09-27 10:01:33 +0800

    added slider in profile

--- frontend/src/components/cars/index.js
+++ frontend/src/components/cars/index.js
@@ -4,57 +4,45 @@ import Moment from "react-moment";
 import { useSelector } from "react-redux";
 import { historyReducer } from "../../functions/reducers";
 import CreateBooking from "../../pages/profile/createHistory";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import "./style.css";
 
 const Car = ({ car, user }) => {
-  const settings = {
-    dots: false,
-    infinite: false,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    speed: 500,
-    arrows: true,
-  };
-  
+
+
   const [visible, setVisible] = useState(false);
 
   return (
-    <div className="car_container">
-      <Moment fromNow interval={30}>
-        {car.createdAt}
-      </Moment>
-      <div className="car_image">
-        <img src={car.image} alt="" />
-      </div>
+      <div className="car_container">
+        <Moment fromNow interval={30}>
+          {car.createdAt}
+        </Moment>
+        <div className="car_image">
+          <img src={car.image} alt="" />
+        </div>
 
-      <Slider {...settings} className="slider">
         <div className="car_text">
           <h3>{car.car_brand}</h3>
           <span>{car.car_model}</span>
           <span>{new Date(car.car_year).getFullYear()}</span>
           <span>{car.car_variant}</span>
         </div>
-      </Slider>
 
-      <div className="btns_container">
-        <button
-          className="red_btn"
-          onClick={() => {
-            setVisible(true);
-          }}
-        >
-          BOOK A SERVICE
-        </button>
-        <button className="gray_btn">REPAIR STATUS</button>
-        <button className="gray_btn">SERVICE HISTORY</button>
+        <div className="btns_container">
+          <button
+            className="red_btn"
+            onClick={() => {
+              setVisible(true);
+            }}
+          >
+            BOOK A SERVICE
+          </button>
+          <button className="gray_btn">REPAIR STATUS</button>
+          <button className="gray_btn">SERVICE HISTORY</button>
+        </div>
+        {visible && (
+          <CreateBooking user={user} car={car} setVisible={setVisible} />
+        )}
       </div>
-      {visible && (
-        <CreateBooking user={user} car={car} setVisible={setVisible} />
-      )}
-    </div>
   );
 };
 
--- frontend/src/pages/profile/profile_nav/Cars.js
+++ frontend/src/pages/profile/profile_nav/Cars.js
@@ -5,16 +5,33 @@ import Car from "../../../components/cars";
 import { createCar } from "../../../functions/createCar";
 import { carsReducer } from "../../../functions/reducers";
 import AddCar from "../addCar";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Cars = ({ profile }) => {
   const { user } = useSelector((state) => ({ ...state }));
   const [visible, setVisible] = useState(false);
 
+  const settings = {
+    dots: false,
+    infinite: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 500,
+    arrows: true,
+  };
+
   return (
     <div className="cars_container">
-      {profile.cars &&
-        profile.cars.length &&
-        profile.cars.map((car) => <Car key={car._id} car={car} user={user} />)}
+      <Slider {...settings} className="slider">
+        {profile.cars &&
+          profile.cars.length &&
+          profile.cars.map((car) => (
+            <Car key={car._id} car={car} user={user} />
+          ))}
+      </Slider>
+
       <button
         className="orange_btn float_right"
         onClick={() => {
