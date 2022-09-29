3beecc47bd58910fd617252c0c21c60e20f10763
Arbie Abesamis <arbieabesamis@gmail.com>
2022-09-29 19:24:52 +0800

    make it better

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 const fileupload = require("express-fileupload");
 const { readdirSync } = require("fs");
 const dotenv = require("dotenv");
+const path = require("path");
+
 dotenv.config();
 
 const app = express();
@@ -28,7 +30,15 @@ mongoose
   .then(() => console.log("database connected successfully"))
   .catch((err) => console.log("error connecting to mongodb", err));
 
-const PORT = process.env.PORT;
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("Server is running");
 });
